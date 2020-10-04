25e3b9fa5e3b0b38da0683f5d92d5116920904d6
m.maselli <m.maselli@reply.it>
2020-10-04 16:42:52 +0200

    remove mock

--- src/app/mock-branches.ts
+++
@@ -1,9 +0,0 @@
-import { Branch } from './model/branch';
-import { Address } from './model/address';
-import { GeoLocation } from './model/geoLocation';
-
-export const BRANCHES: Branch[] = [
-    new Branch("ING", "0", new Address("Sterrebos","2","5344 AM","Oss", new GeoLocation("1","2"))),
-    new Branch("ING", "1", new Address("Sterre","5","5555 AM","Oss", new GeoLocation("1","2")))
-];
-
